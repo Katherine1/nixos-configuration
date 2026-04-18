35f4d8c2c57b18929fb1499f34a1ca6177740943
Emily Marriott <emilylynmarriott@proton.me>
2026-04-18 09:30:56 -0500

    Set up Home Assistant on Wolf

--- hosts/wolf/services.nix
+++ hosts/wolf/services.nix
@@ -2,9 +2,10 @@
 {
     imports = [
         ../../modules/services/caddy.nix
-        ../../modules/services/fstrim.nix
-        ../../modules/services/pihole.nix
         ../../modules/services/docker.nix
+        ../../modules/services/fstrim.nix
+        ../../modules/services/home-assistant.nix
+        ../../modules/services/pihole.nix
         ../../modules/services/ssh.nix
         #../../modules/services/syncthing.nix
     ];
--- modules/services/home-assistant.nix
+++ modules/services/home-assistant.nix
@@ -1,20 +1,31 @@
 { config, pkgs, inputs, ... }:
 {
-    services.home-assistant = {
-        enable = true;
-        openFirewall = true;
+    services = {
+        home-assistant = {
+            enable = true;
+            openFirewall = true;
 
-        config = {
-            homeassistant = {
-                latitude = 43.11;
-                longitude = -88.34;
-                name = "Home";
-                temperature_unit = "F";
-                time_zone = "America/Chicago";
-                unit_system = "us_customary";
+            config = {
+                homeassistant = {
+                    latitude = 43.11;
+                    longitude = -88.34;
+                    name = "Home";
+                    temperature_unit = "F";
+                    time_zone = "America/Chicago";
+                    unit_system = "us_customary";
+                };
+
+                server_port = 8123;
             };
 
-            server_port = 8123;
+            extraComponents = [
+                "default_config"
+                "met"
+                "radio_browser"
+                "mqtt"
+            ];
         };
+
+        mosquitto.enable = true;
     };
 }
