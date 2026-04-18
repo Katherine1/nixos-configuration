14509c060d490f906377278455ee53c44b302a5c
Emily Marriott <emilylynmarriott@proton.me>
2026-04-18 07:36:42 -0500

    Add basic home assistant config

---
+++ modules/services/home-assistant.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, inputs, ... }:
+{
+    services.home-assistant = {
+        enable = true;
+        openFirewall = true;
+
+        config = {
+            homeassistant = {
+                latitude = 43.11;
+                longitude = -88.34;
+                name = "Home";
+                temperature_unit = "F";
+                time_zone = "America/Chicago";
+                unit_system = "us_customary";
+            };
+
+            server_port = 8123;
+        };
+    };
+}
