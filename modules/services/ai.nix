fa8b050bf03dad89e8d19357256fdf4d412432c0
Emily Marriott <emilylynmarriott@proton.me>
2026-05-04 21:05:59 -0500

    Initial config for tabby

--- modules/services/ai.nix
+++ modules/services/ai.nix
@@ -1,10 +1,20 @@
 { config, pkgs, inputs, ... }:
 {
-    services.open-webui = {
-        enable = false;
-        host = "127.0.0.1";
-        port = 8080;
-        openFirewall = true;
+    services = {
+        open-webui = {
+            enable = false;
+            host = "127.0.0.1";
+            port = 8080;
+            openFirewall = true;
+        };
+        
+        tabby = {
+            enable = false;
+            host = "127.0.0.1";
+            port = 11029;
+            acceleration = "cuda";
+            usageCollection = false;
+        };
     };
     
     environment.systemPackages = with pkgs; [
